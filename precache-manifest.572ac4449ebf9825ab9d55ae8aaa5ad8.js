8e10249fee26ee47bedf0c8f3d53a5e8cde36c36
MoezKouni <mo.elkouni@gmail.com>
2019-07-16 11:09:13 +0100

    Updates

---
+++ precache-manifest.572ac4449ebf9825ab9d55ae8aaa5ad8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25693f3cc178f9271ee7efc2b1ed2834",
+    "url": "/github.io/my-articles/index.html"
+  },
+  {
+    "revision": "15049f644e18442bef96",
+    "url": "/github.io/my-articles/static/css/main.d0ea77c8.chunk.css"
+  },
+  {
+    "revision": "22fd495ff243d2ab8ed1",
+    "url": "/github.io/my-articles/static/js/2.7f87a15b.chunk.js"
+  },
+  {
+    "revision": "15049f644e18442bef96",
+    "url": "/github.io/my-articles/static/js/main.82b2c47d.chunk.js"
+  },
+  {
+    "revision": "b4c2df1d465a062f2ac4",
+    "url": "/github.io/my-articles/static/js/runtime~main.79668e08.js"
+  }
+]);
